dff6eb8d8c98a93ccb5f20c8f6c4966e24698dc9
Shed <shemadaniel10@gmail.com>
2022-07-16 04:20:15 +0200

    .

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// num key
+var one = document.getElementById('one');
+var two = document.getElementById('two');
+var three = document.getElementById('three');
+var four = document.getElementById('four');
+var five = document.getElementById('five');
+var six = document.getElementById('six');
+var seven = document.getElementById('seven');
+var eight = document.getElementById('eight');
+var nine = document.getElementById('nine');
+var zero = document.getElementById('one');
+// operators
+var plus = document.getElementById('plus');
+var minus = document.getElementById('minus');
+var point = document.getElementById('point');
+var division = document.getElementById('division');
+// other
+var product = document.getElementById('product');
+var del = document.getElementById('del');
+var reset = document.getElementById('reset');
+var equal = document.getElementById('equal');
+
+one.addEventListener('click', () =>{
+    console.log('one')
+})
