c40740846c3bb9e7bd32c8077ccd9d42fbea60b5
Shed <shemadaniel10@gmail.com>
2022-07-16 04:30:05 +0200

    .

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var six = document.getElementById('six');
 var seven = document.getElementById('seven');
 var eight = document.getElementById('eight');
 var nine = document.getElementById('nine');
-var zero = document.getElementById('one');
+var zero = document.getElementById('zero');
 // operators
 var plus = document.getElementById('plus');
 var minus = document.getElementById('minus');
@@ -19,7 +19,49 @@ var product = document.getElementById('product');
 var del = document.getElementById('del');
 var reset = document.getElementById('reset');
 var equal = document.getElementById('equal');
+var output = document.getElementById('output')
 
 one.addEventListener('click', () =>{
-    console.log('one')
+    output.innerHTML = '1';
+})
+two.addEventListener('click', () =>{
+    output.innerHTML = '2';
+})
+three.addEventListener('click', () =>{
+    output.innerHTML = '3';
+})
+four.addEventListener('click', () =>{
+    output.innerHTML = '4';
+})
+five.addEventListener('click', () =>{
+    output.innerHTML = '5';
+})
+six.addEventListener('click', () =>{
+    output.innerHTML = '6';
+})
+seven.addEventListener('click', () =>{
+    output.innerHTML = '7';
+})
+eight.addEventListener('click', () =>{
+    output.innerHTML = '8';
+})
+nine.addEventListener('click', () =>{
+    output.innerHTML = '9';
+})
+zero.addEventListener('click', () =>{
+    output.innerHTML = '0';
+})
+
+// Operator
+minus.addEventListener('click', () =>{
+    output.innerHTML = '-';
+})
+plus.addEventListener('click', () =>{
+    output.innerHTML = '+';
+})
+division.addEventListener('click', () =>{
+    output.innerHTML = '/';
+})
+product.addEventListener('click', () =>{
+    output.innerHTML = 'x';
 })
