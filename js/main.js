bbe0cb3cd8f24825dfdb5c1633a50bec665c75c4
Shed <shemadaniel10@gmail.com>
2022-07-18 01:42:50 +0200

    .

--- js/main.js
+++ js/main.js
@@ -2,8 +2,12 @@ var operator = document.querySelectorAll('.operator');
 for(var i = 0;i<operator.length;i++){
     // console.log(operator[i]);
     operator[i].addEventListener('click', function (){
-        // alert('hooray')
-        // alert("Operator is "+this.innerHTML);
         console.log("Operator is "+this.innerHTML);
     })
+}
+var numbers = document.querySelectorAll('[data-number]');
+for(var i = 0 ; i < numbers.length; i++){
+    numbers[i].onclick = function (){
+        console.log('This is '+this.innerHTML)
+    }
 }
